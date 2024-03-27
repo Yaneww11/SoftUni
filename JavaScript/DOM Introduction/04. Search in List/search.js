b1b6243903b22db8ce6daed9bd93dcd7bbc1e49a
Yane Yanev <93374652+Yaneww11@users.noreply.github.com>
2024-03-27 19:32:21 +0200

    Update search.js

--- JavaScript/DOM Introduction/04. Search in List/search.js
+++ JavaScript/DOM Introduction/04. Search in List/search.js
@@ -1,3 +1,22 @@
 function search() {
-   // TODO
+   const elementSearchField = document.getElementById("searchText");
+   const searchText = elementSearchField.value.toLowerCase();
+   const elUl = document.getElementById("towns");
+   const elResult = document.getElementById("result");
+   let countMatches = 0;
+   const towns = Array.from(elUl.children);
+
+    for (const town of towns) {
+        if (town.textContent.toLowerCase().includes(searchText)){
+            town.style.textDecoration = "underline";
+            town.style.fontWeight = "bold";
+            countMatches++;
+        }
+        else {
+            town.style.textDecoration = "none";
+            town.style.fontWeight = "normal";
+        }
+    }
+
+    elResult.textContent = `${countMatches} matches found`;
 }
