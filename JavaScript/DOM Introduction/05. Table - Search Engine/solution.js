d73a1c45f5813a2a80412c92823d573744f2054a
Yane Yanev <93374652+Yaneww11@users.noreply.github.com>
2024-03-27 19:32:49 +0200

    Update solution.js

--- JavaScript/DOM Introduction/05. Table - Search Engine/solution.js
+++ JavaScript/DOM Introduction/05. Table - Search Engine/solution.js
@@ -2,7 +2,22 @@ function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
+      const trElements = document.querySelectorAll('table.container tbody tr');
+      const searchFieldElement = document.getElementById('searchField');
+      const searchText = searchFieldElement.value;
 
+      for (const trElement of trElements) {
+         const tdElements = trElement.querySelectorAll('td');
+         for (const td of tdElements) {
+            if (td.textContent.includes(searchText)){
+               trElement.classList.add('select');
+               break;
+            }
+            else {
+               trElement.classList.remove('select');
+            }
+         }
+      }
+      searchFieldElement.value = "";
    }
 }
