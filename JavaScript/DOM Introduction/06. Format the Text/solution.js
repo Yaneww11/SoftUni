687e5cf0827c828a59405f9b00a2d60de6f3235a
Yane Yanev <93374652+Yaneww11@users.noreply.github.com>
2024-03-27 19:33:13 +0200

    Update solution.js

--- JavaScript/DOM Introduction/06. Format the Text/solution.js
+++ JavaScript/DOM Introduction/06. Format the Text/solution.js
@@ -1,3 +1,22 @@
 function solve() {
-  //TODO
+    const outputElement = document.getElementById('output');
+    const textElement = document.getElementById("input");
+    const text = textElement.value;
+
+    const result = text
+        .split('.')
+        .filter(element => !!element)
+        .reduce((result, sentence, i) => {
+          const resultIndex = Math.floor(i / 3);
+          if (!result[resultIndex]){
+              result[resultIndex] = [];
+          }
+          result[resultIndex].push(sentence.trim())
+
+          return result
+        }, [])
+        .map(sentence => `<p>${sentence.join('. ')}.</p>`)
+        .join("\n")
+
+    outputElement.innerHTML = result;
 }
