69dbc876918e81a904306f111c720d8d4d3b969b
Yane Yanev <93374652+Yaneww11@users.noreply.github.com>
2024-03-27 19:33:52 +0200

    Update solution.js

--- JavaScript/DOM Introduction/09. Number Convertor/solution.js
+++ JavaScript/DOM Introduction/09. Number Convertor/solution.js
@@ -1,4 +1,53 @@
 function solve() {
+    const inputNumberElement = document.getElementById('input');
+    const selectMenuFromElement = document.getElementById('selectMenuFrom');
+    const selectMenuToElement = document.getElementById('selectMenuTo');
+    const resultElement = document.getElementById('result');
+    const buttonConvert = document.querySelector('button');
 
-    //TODO...
+    //Create elements
+    const binaryOptionElement = selectMenuToElement.querySelector('option');
+    binaryOptionElement.value = 'binary';
+    binaryOptionElement.textContent = 'Binary';
+
+    const hexadecimalOptionElement = document.createElement('option');
+    hexadecimalOptionElement.value = 'hexadecimal';
+    hexadecimalOptionElement.textContent = 'Hexadecimal';
+    selectMenuToElement.appendChild(hexadecimalOptionElement);
+
+    function convertDecimalToBinary(number){
+        let result = "";
+        while (number !== 0){
+            result += number % 2;
+            number = Math.floor(number / 2)
+        }
+        return result.split('').reverse().join('');
+    }
+
+    function convertDecimalToHexadecimal(number){
+        const hexadecimalSymbols = ['A', 'B', 'C', 'D', 'E', 'F']
+        let result = "";
+        while (number !== 0){
+            let remainder = number % 16;
+            if (remainder > 9){
+                remainder = hexadecimalSymbols[remainder - 10];
+            }
+            result += remainder;
+            number = Math.floor(number / 16);
+        }
+
+        return result.split('').reverse().join('');
+    }
+
+    buttonConvert.addEventListener('click', () => {
+        const number = Number(inputNumberElement.value);
+        let result = "";
+        if (selectMenuToElement.value === 'hexadecimal'){
+            result = convertDecimalToHexadecimal(number);
+        }
+        else {
+            result = convertDecimalToBinary(number);
+        }
+        resultElement.value = result;
+    })
 }
