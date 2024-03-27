8dff00f271ff7519e71a0978cfea046f814d91c5
Yane Yanev <93374652+Yaneww11@users.noreply.github.com>
2024-03-27 19:32:00 +0200

    Update accordion.js

--- JavaScript/DOM Introduction/03. Accordion/accordion.js
+++ JavaScript/DOM Introduction/03. Accordion/accordion.js
@@ -1,3 +1,14 @@
 function toggle() {
-    console.log('TODO:...');
+    const hide_text = document.getElementById("extra");
+    const elementButton = document.querySelector('.head span.button');
+    let currentButtonText = elementButton.textContent;
+
+    if (currentButtonText === 'More'){
+        hide_text.style.display = 'block';
+        elementButton.textContent = 'Less';
+    }
+    if (currentButtonText === 'Less'){
+        hide_text.style.display = 'none';
+        elementButton.textContent = 'More';
+    }
 }
